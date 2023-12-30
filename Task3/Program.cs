d2ffce85574488fa763f7e8dec84d0b17120162e
A-NB <bel-an510@yandex.ru>
2023-12-30 18:26:40 +0300

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Знакомство с языками программирования (семинары)
+// Урок 7. Рекурсия
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+char[] GetRandomArrayOfChar()
+{
+    string st = "0123456789abcdefjhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯЁ~@#$%^&!№;%:?*()_+/-.,\"<>";
+    Random rnd = new Random();
+    char[] array = new char[rnd.Next(5, 10)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = st[rnd.Next(0, st.Length)];
+    }
+    return array;
+}
+
+void PrintArray(char[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+void PrintReverseArray(char[] array, int index)
+{
+    System.Console.Write(array[index] + " ");
+    if (index > 0)
+    {
+        PrintReverseArray(array, index - 1);
+    }
+}
+
+char[] array = GetRandomArrayOfChar();
+PrintArray(array);
+PrintReverseArray(array, array.Length - 1);
